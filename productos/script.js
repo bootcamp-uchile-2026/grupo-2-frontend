c4e63ceed51fb1751ff046d4e768dc4006143d1b
Cristina Ortiz <cristina.ortiz1297@gmail.com>
2026-08-23 14:46:16 -0400

    Se ha mejorado el menú desplegable

--- productos/script.js
+++ productos/script.js
@@ -1,11 +1,11 @@
-const boton = document.getElementById('boton-categorias'); 
-const lista = document.querySelector('.lista-categorias');
-//Evento de escucha para el botón categorias
-boton.addEventListener('click',() => {
-    lista.classList.toggle('activo');
+//Evento para desplegar el menú de categorías 
+document.addEventListener('DOMContentLoaded', () => {
+  const boton = document.getElementById('boton-categorias');
+  const menu = document.getElementById('menu-categorias');
+
+  if (boton && menu) {
+    boton.addEventListener('click', () => {
+      menu.classList.toggle('activo');
+    });
+  }
 });
-document.addEventListener('click', (evento) => {
-if (!boton.contains(evento.target) && !lista.contains(evento.target)){
-    lista.classList.remove ('activo');
-}
-})
