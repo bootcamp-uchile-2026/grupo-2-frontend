855501a437158b156f0058f2d45e97ba8c35a2d2
Cristina Ortiz <cristina.ortiz1297@gmail.com>
2026-08-23 15:02:52 -0400

    Se ha modificado la apariencia de las letras del menú

--- productos/script.js
+++ productos/script.js
@@ -1,11 +1,11 @@
 //Evento para desplegar el menú de categorías 
 document.addEventListener('DOMContentLoaded', () => {
-  const boton = document.getElementById('boton-categorias');
-  const menu = document.getElementById('menu-categorias');
+    const boton = document.getElementById('boton-categorias');
+    const menu = document.getElementById('menu-categorias');
 
-  if (boton && menu) {
+if (boton && menu) {
     boton.addEventListener('click', () => {
-      menu.classList.toggle('activo');
+    menu.classList.toggle('activo');
     });
-  }
+}
 });
